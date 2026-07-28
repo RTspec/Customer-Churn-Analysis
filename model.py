f2118446c63beb36de600b0c072794b85a4be8fd
unknown <mousumisingh801@gmail.com>
2026-07-28 15:10:11 +0530

    Added label encoding and logistic regression model

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import pickle
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+
 from sklearn.metrics import (
     accuracy_score,
     precision_score,
@@ -11,13 +13,38 @@ from sklearn.metrics import (
     classification_report
 )
 
+
 # Load Dataset
 df = pd.read_csv("dataset/customer_churn_processed.csv")
 
 print("Dataset Loaded Successfully")
 print("Shape:", df.shape)
 
+
+# ==========================
+# Label Encoding
+# ==========================
+
+encoder = LabelEncoder()
+
+categorical_columns = [
+    "gender",
+    "Partner",
+    "Dependents",
+    "Contract",
+    "Churn"
+]
+
+for col in categorical_columns:
+    df[col] = encoder.fit_transform(df[col])
+
+print("Label Encoding Done")
+
+
+# ==========================
 # Features and Target
+# ==========================
+
 X = df[
     [
         "gender",
@@ -33,7 +60,11 @@ X = df[
 
 y = df["Churn"]
 
-# Train-Test Split
+
+# ==========================
+# Train Test Split
+# ==========================
+
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -41,24 +72,39 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
+
 print("Training Model...")
 
+
+# ==========================
 # Logistic Regression Model
+# ==========================
+
 model = LogisticRegression(max_iter=1000)
+
 
 # Train Model
 model.fit(X_train, y_train)
 
 print("Model Trained Successfully!")
 
+
+# ==========================
 # Prediction
+# ==========================
+
 y_pred = model.predict(X_test)
 
+
+# ==========================
 # Evaluation
+# ==========================
+
 accuracy = accuracy_score(y_test, y_pred)
 precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
+
 
 print("\n===== MODEL PERFORMANCE =====")
 print("Accuracy :", accuracy)
@@ -66,11 +112,22 @@ print("Precision:", precision)
 print("Recall   :", recall)
 print("F1 Score :", f1)
 
+
 print("\n===== CLASSIFICATION REPORT =====")
 print(classification_report(y_test, y_pred))
 
+
+# ==========================
 # Save Model
+# ==========================
+
 with open("churn_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
-print("\nModel saved successfully as churn_model.pkl")
+
+# Save Encoder
+with open("encoder.pkl", "wb") as f:
+    pickle.dump(encoder, f)
+
+
+print("\nModel and Encoder Saved Successfully!")
